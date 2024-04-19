1ac8f6270def22ac196475efc42efb2754fec462
Ertholin <ertholinulysse@gmail.com>
2024-04-19 05:43:13 -0400

    no-feat: last commit before submitting

--- script.js
+++ script.js
@@ -9,16 +9,3 @@ document.querySelectorAll('.overlay').forEach(function(overlay) {
 });
 
 
-var monCanvas = document.getElementById('dessin');
-var ctx = monCanvas.getContext('2d');
-
-ctx.lineWidth = 5;
-ctx.fillStyle = "#FDAD52";
-ctx.strokeStyle = "#FFFFFF";
-
-ctx.beginPath();
-
-ctx.arc(153, 53, 50, 0, Math.PI*2, true);
-
-ctx.fill();
-ctx.stroke();
